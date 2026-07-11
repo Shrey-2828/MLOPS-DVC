87e87b3f55a97a15dec98be8278b3b934161bf04
Shrey-2828 <ldce.2324@gmail.com>
2026-07-11 16:44:00 +0530

    third version of data

--- code.py
+++ code.py
@@ -12,6 +12,9 @@ df=pd.DataFrame(data)
 new_row_loc = {'name': 'GF1', 'age': 20, 'id': 103}
 df.loc[len(df.index)] = new_row_loc
 
+new_row_loc = {'name': 'GF2', 'age': 27, 'id': 104}
+df.loc[len(df.index)] = new_row_loc
+
 
 dir_name='data'
 dir1=os.makedirs(dir_name,exist_ok=True)
